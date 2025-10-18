39b142381058898579a66fada3e458c3f0ede9e8
nahidsukurlu <nahedshukurlu@gmail.com>
2025-10-19 03:28:32 +0400

    Fix deploy issues: CNAME and PORT configuration

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const path = require('path');
 const fs = require('fs');
 
 const app = express();
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 10000;
 
 // Middleware
 app.use(cors());
